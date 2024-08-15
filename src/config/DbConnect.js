2226543d60a82b052e711cf0a0ac28551f0b5b33
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-08-14 20:43:05 -0400

    feat: create DbConnection file, add dotenv package and create DB connection methods

---
+++ src/config/DbConnect.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import mongoose, { mongo } from "mongoose";
+
+const databaseLogin = process.env.DATABASE_LOGIN;
+const databasePwd = encodeURIComponent(process.env.DATABASE_PASSWORD); // encodeURIComponent is called in case password has special characters
+
+async function connectDatabase() {
+  mongoose.connect(
+    `mongodb+srv://${databaseLogin}:${databasePwd}@mycluster.g57a8jt.mongodb.net/OrganizadorFinanceiro?retryWrites=true&w=majority&appName=MyCluster`
+  );
+
+  return mongoose.connection;
+}
+
+export default connectDatabase;
