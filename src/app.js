a57656bf76ee5492b663635531298a667b987de0
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-08-14 20:43:05 -0400

    feat: create DbConnection file, add dotenv package and create DB connection methods

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import express from "express";
 import { OK_STATUS } from "./Models/Constants.js";
+import connectDatabase from "./config/DbConnect.js";
+
+const dbConnect = await connectDatabase();
+
+// DB connection error event method
+dbConnect.on("error", (error) => {
+  console.error("Database error: ", error);
+});
+
+// DB Connection event method
+dbConnect.once("open", () => {
+  console.log("Dabatase connection successful");
+});
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(OK_STATUS).send("Express API working...");
 });
-
-// mongodb+srv://MLSoftwareAdmin:<password>@mycluster.g57a8jt.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster
 
 export default app;
