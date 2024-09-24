1c0d798b9982650b282b572a84f0f42eaeb31ef5
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-09-23 20:43:16 -0400

    feat: create getById, put and delete endpoints

--- src/Controllers/ExpenseController.js
+++ src/Controllers/ExpenseController.js
@@ -7,8 +7,38 @@ import {
 
 class ExpenseController {
   static async getAllExpenses(req, res) {
-    const expenseList = await expense.find({});
-    res.status(OK_STATUS).json(expenseList);
+    try {
+      const expenseList = await expense.find({});
+      res.status(OK_STATUS).json(expenseList);
+    } catch (error) {
+      res
+        .status(INTERNAL_SERVER_ERROR_STATUS)
+        .json({ message: `${error.message} - request error` });
+    }
+  }
+
+  static async getExpenseById(req, res) {
+    try {
+      const id = req.params.id;
+      const expenseFound = await expense.findById(id);
+      res.status(OK_STATUS).json(expenseFound);
+    } catch (error) {
+      res
+        .status(INTERNAL_SERVER_ERROR_STATUS)
+        .json({ message: `${error.message} - request error` });
+    }
+  }
+
+  static async updateExpense(req, res) {
+    try {
+      const id = req.params.id;
+      await expense.findByIdAndUpdate(id, req.body);
+      res.status(OK_STATUS).json({ message: "Expense updated!" });
+    } catch (error) {
+      res
+        .status(INTERNAL_SERVER_ERROR_STATUS)
+        .json({ message: `${error.message} - update request error` });
+    }
   }
 
   static async insertExpense(req, res) {
@@ -21,6 +51,18 @@ class ExpenseController {
       res
         .status(INTERNAL_SERVER_ERROR_STATUS)
         .json({ message: `${erro.message} - fail to insert expense` });
+    }
+  }
+
+  static async deleteExpenseById(req, res) {
+    try {
+      const id = req.params.id;
+      await expense.findByIdAndDelete(id);
+      res.status(OK_STATUS).json({ message: "expense deleted!" });
+    } catch (error) {
+      res
+        .status(INTERNAL_SERVER_ERROR_STATUS)
+        .json({ message: `${error.message} - delete request error` });
     }
   }
 }
