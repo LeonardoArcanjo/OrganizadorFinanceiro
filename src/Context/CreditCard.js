52b6b26e3a3c3d18fd90dc193e876c3cf96cb53c
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-10-10 20:11:16 -0400

    feat: create CreditCardController.js and create date property in CreditCard model

--- src/Context/CreditCard.js
+++ src/Context/CreditCard.js
@@ -1,17 +1,19 @@
 import mongoose, { Types } from "mongoose";
 import { ExpenseSchema } from "./Expense.js";
+import { type } from "express/lib/response.js";
 const { Schema } = mongoose;
 
-const CreditCardSchema = new Schema(
+const CreditCardExpenseSchema = new Schema(
   {
     id: { type: Types.ObjectId },
     bankName: { type: String, required: true },
-    installments: { type: Number, required },
+    installments: { type: Number, required: true },
+    date: { type: Date, required: true },
     expense: ExpenseSchema,
   },
   { versionKey: false }
 );
 
-const creditCard = mongoose.model("CreditCard", CreditCardSchema);
+const CreditCardExpense = mongoose.model("CreditCard", CreditCardExpenseSchema);
 
-export default creditCard;
+export default CreditCardExpense;
