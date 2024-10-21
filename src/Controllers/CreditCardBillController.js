30ad2ee8e8bce65361ac950cc95478ffc4e241ac
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-10-21 19:28:03 -0400

    feat: create CreditCardBillController.js file

---
+++ src/Controllers/CreditCardBillController.js
@@ -0,0 +1,10 @@
+import CreditCardBill from "../Context/CreditCardBill.js";
+import {
+  CREATE_STATUS,
+  INTERNAL_SERVER_ERROR_STATUS,
+  OK_STATUS,
+} from "../Models/Constants";
+
+class CreditCardBillConstroller {
+  static async getAllCreditCardBill(req, res) {}
+}
