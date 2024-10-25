27040d05aa1c816822525cf94a3a12e2563c6d82
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-10-25 11:45:48 -0400

    feat: create Error Handler middleware in App.js and insert middleware in ExpenseController.js

--- src/Controllers/ExpenseController.js
+++ src/Controllers/ExpenseController.js
@@ -1,68 +1,57 @@
-import expense from "../Context/Expense.js";
+import { expense } from "../Context/Expense.js";
 import {
   CREATE_STATUS,
-  INTERNAL_SERVER_ERROR_STATUS,
   OK_STATUS,
 } from "../Models/Constants.js";
 
 class ExpenseController {
-  static async getAllExpenses(req, res) {
+  static async getAllExpenses(req, res, next) {
     try {
       const expenseList = await expense.find({});
       res.status(OK_STATUS).json(expenseList);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - request error` });
+      next(error);
     }
   }
 
-  static async getExpenseById(req, res) {
+  static async getExpenseById(req, res, next) {
     try {
       const id = req.params.id;
       const expenseFound = await expense.findById(id);
       res.status(OK_STATUS).json(expenseFound);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - request error` });
+      next(error);
     }
   }
 
-  static async updateExpense(req, res) {
+  static async updateExpense(req, res, next) {
     try {
       const id = req.params.id;
       await expense.findByIdAndUpdate(id, req.body);
       res.status(OK_STATUS).json({ message: "Expense updated!" });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - update request error` });
+      next(error);
     }
   }
 
-  static async insertExpense(req, res) {
+  static async insertExpense(req, res, next) {
     try {
       const newExpense = await expense.create(req.body);
       res
         .status(CREATE_STATUS)
         .json({ message: "Inserted successfully", expense: newExpense });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - fail to insert expense` });
+      next(error);
     }
   }
 
-  static async deleteExpenseById(req, res) {
+  static async deleteExpenseById(req, res, next) {
     try {
       const id = req.params.id;
       await expense.findByIdAndDelete(id);
       res.status(OK_STATUS).json({ message: "expense deleted!" });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - delete request error` });
+      next(error);
     }
   }
 }
