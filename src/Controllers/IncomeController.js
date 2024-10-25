b7083624c9bde3c0572768ff7735c9ec1e302a0d
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-10-25 12:15:18 -0400

    feat: create IncomeRoutes.js and add error request handling in IncomeController.js endpoint methods

--- src/Controllers/IncomeController.js
+++ src/Controllers/IncomeController.js
@@ -1,36 +1,31 @@
 import income from "../Context/Income.js";
 import {
   CREATE_STATUS,
-  INTERNAL_SERVER_ERROR_STATUS,
   OK_STATUS,
 } from "../Models/Constants";
 
 class IncomeController {
-  static async getAllIncomes(req, res) {
+  static async getAllIncomes(req, res, next) {
     try {
       const incomeList = await income.find({});
       res.status(OK_STATUS).json(incomeList);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - request error` });
+      next(error)
     }
   }
 
-  static async getIncomeById(req, res) {
+  static async getIncomeById(req, res, next) {
     try {
       const id = req.params.id;
       const incomeFound = await income.findById(id);
 
       res.status(OK_STATUS).json(incomeFound);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - request error` });
+      next(error)
     }
   }
 
-  static async getIncomeByCategory(req, res) {
+  static async getIncomeByCategory(req, res, next) {
     try {
       const category = req.params.category;
       const incomeByCategoryList = await income.find({
@@ -38,46 +33,38 @@ class IncomeController {
       });
       res.status(OK_STATUS).json(incomeByCategoryList);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - request error` });
+      next(error);
     }
   }
 
-  static async updateIncome(req, res) {
+  static async updateIncome(req, res, next) {
     try {
       const id = req.params.id;
       await income.findByIdAndUpdate(id, req.body);
       res.status(OK_STATUS).json({ message: "Income Updated!" });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - update request error` });
+      next(error)
     }
   }
 
-  static async insertIncome(req, res) {
+  static async insertIncome(req, res, next) {
     try {
       const newIncome = await income.create(req.body);
       res
         .status(CREATE_STATUS)
         .json({ message: "Income created successfully", income: newIncome });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - fail to create income` });
+      next(error);
     }
   }
 
-  static async deleteIncome(req, res) {
+  static async deleteIncome(req, res, next) {
     try {
       const id = req.params.id;
       await income.findByIdAndDelete(id);
       res.status(OK_STATUS).json({ message: "Income deleted!" });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - delete request error` });
+      next(error)
     }
   }
 }
