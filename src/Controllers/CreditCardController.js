8db1b95037b87e220a55bf76cd1103cac2e7d19f
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-10-25 12:58:58 -0400

    feat: add .vscode at gitignore file and add error request handler in CreditCardController.js

--- src/Controllers/CreditCardController.js
+++ src/Controllers/CreditCardController.js
@@ -6,18 +6,16 @@ import {
 } from "../Models/Constants";
 
 class CrediCardController {
-  static async getAllCreditCardExpenses(req, res) {
+  static async getAllCreditCardExpenses(req, res, next) {
     try {
       const ccExpenseList = await CreditCardExpense.find({});
       res.status(OK_STATUS).json(ccExpenseList);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - request error` });
+      next(error);
     }
   }
 
-  static async getCCExpensesByBank(req, res) {
+  static async getCCExpensesByBank(req, res, next) {
     try {
       const bankName = req.params.bankName;
       const ccExpenseListByBank = await CreditCardExpense.find({
@@ -25,37 +23,31 @@ class CrediCardController {
       });
       res.status(OK_STATUS).json(ccExpenseListByBank);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message}` });
+      next(error);
     }
   }
 
-  static async getCCExpensesById(req, res) {
+  static async getCCExpensesById(req, res, next) {
     try {
       const ccExpenseId = req.params.id;
       const ccExpense = await CreditCardExpense.findById(ccExpenseId);
       res.status(OK_STATUS).json(ccExpense);
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message}` });
+      next(error);
     }
   }
 
-  static async updateCCExpensesById(req, res) {
+  static async updateCCExpensesById(req, res, next) {
     try {
       const ccExpenseId = req.params.id;
       await CreditCardExpense.findByIdAndUpdate(ccExpenseId, req.body);
       res.status(OK_STATUS).json({ message: "Credit Card Expense Updated!" });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message}` });
+      next(error);
     }
   }
 
-  static async insertCCExpense(req, res) {
+  static async insertCCExpense(req, res, next) {
     try {
       const newCCExpense = await CreditCardExpense.create(req.body);
       res.status(CREATE_STATUS).json({
@@ -63,21 +55,17 @@ class CrediCardController {
         creditCardExpense: newCCExpense,
       });
     } catch (error) {
-      res.status(INTERNAL_SERVER_ERROR_STATUS).json({
-        message: `${error.message} - fail to create Credit Card Expense`,
-      });
+      next(error);
     }
   }
 
-  static async deleteCCExpense(req, res) {
+  static async deleteCCExpense(req, res, next) {
     try {
       const id = req.params.id;
       await CreditCardExpense.findByIdAndDelete(id);
       res.status(OK_STATUS).json({ message: "Credit Card Expense deleted!" });
     } catch (error) {
-      res
-        .status(INTERNAL_SERVER_ERROR_STATUS)
-        .json({ message: `${error.message} - delete request error` });
+      next(error);
     }
   }
 }
