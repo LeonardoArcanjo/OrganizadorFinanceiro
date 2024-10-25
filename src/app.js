6c4e822eaca2100c4640907d6352d6824ca72fd6
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-10-25 11:54:45 -0400

    feat: create Error Request Handler file and Middleware path

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import express from "express";
 import connectDatabase from "./Context/DbConnect.js";
 import routes from "./routes/index.js";
-import mongoose from "mongoose";
-import { BAD_REQUEST_STATUS, INTERNAL_SERVER_ERROR_STATUS } from "./Models/Constants.js";
+import ErrorRequestHandler from "./Middleware/ErrorHandler.js";
 
 const dbConnect = await connectDatabase();
 
@@ -21,14 +20,7 @@ app.use(express.json());
 routes(app);
 
 //Error Middleware creation
-// eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  if (error instanceof mongoose.Error.CastError) {
-    res.status(BAD_REQUEST_STATUS).send({ message: "Data request error" })
-  }
-  else {
-    res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: "Internal server error" })
-  }
-})
+
+app.use(ErrorRequestHandler);
 
 export default app;
