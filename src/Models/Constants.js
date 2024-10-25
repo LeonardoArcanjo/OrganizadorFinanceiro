21ef3b1e04ab82f6dcc3f61ca0db5bad95bf6da7
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-10-25 11:45:48 -0400

    feat: create Error Handler middleware in App.js and insert middleware in ExpenseController.js

--- src/Models/Constants.js
+++ src/Models/Constants.js
@@ -2,3 +2,4 @@ export const PORT = 3000;
 export const OK_STATUS = 200;
 export const CREATE_STATUS = 201;
 export const INTERNAL_SERVER_ERROR_STATUS = 500;
+export const BAD_REQUEST_STATUS = 400
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import connectDatabase from "./Context/DbConnect.js";
 import routes from "./routes/index.js";
+import mongoose from "mongoose";
+import { BAD_REQUEST_STATUS, INTERNAL_SERVER_ERROR_STATUS } from "./Models/Constants.js";
 
 const dbConnect = await connectDatabase();
 
@@ -15,6 +17,18 @@ dbConnect.once("open", () => {
 });
 
 const app = express();
+app.use(express.json());
 routes(app);
+
+//Error Middleware creation
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  if (error instanceof mongoose.Error.CastError) {
+    res.status(BAD_REQUEST_STATUS).send({ message: "Data request error" })
+  }
+  else {
+    res.status(INTERNAL_SERVER_ERROR_STATUS).send({ message: "Internal server error" })
+  }
+})
 
 export default app;
