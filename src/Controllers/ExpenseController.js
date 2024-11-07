2b0160ce0f9a004826b1cbcf3f0e60cdaac10b42
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-11-06 20:20:38 -0400

    feat: create filter search endpoint in expenseController.js, create function search query params handler and create NO_CONTENT (204) status code constant

--- src/Controllers/ExpenseController.js
+++ src/Controllers/ExpenseController.js
@@ -1,7 +1,7 @@
 import { expense } from "../Context/Expense.js";
 import ErrorValidation from "../Errors/ErrorValidation.js";
 import NotFound from "../Errors/NotFound.js";
-import { CREATE_STATUS, OK_STATUS } from "../Models/Constants.js";
+import { CREATE_STATUS, NO_CONTENT, OK_STATUS } from "../Models/Constants.js";
 
 class ExpenseController {
   static async getAllExpenses(req, res, next) {
@@ -22,6 +22,21 @@ class ExpenseController {
         res.status(OK_STATUS).json(expenseFound);
       } else {
         next(new NotFound("Expense not found!"));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async searchExpense(req, res, next) {
+    try {
+      let search = searchQueryHandler(req.query);
+      const searchResult = await expense.find(search);
+
+      if (searchResult.length === 0) {
+        res.status(NO_CONTENT).json(searchResult);
+      } else {
+        res.status(OK_STATUS).json(searchResult);
       }
     } catch (error) {
       next(error);
@@ -68,6 +83,22 @@ class ExpenseController {
       next(error);
     }
   }
+}
+
+function searchQueryHandler(params) {
+  const { name, category, minValue, maxValue } = params;
+
+  let search = {};
+
+  if (name) search.name = { $regex: name, $options: "i" };
+  if (category) search.category = category;
+
+  if (minValue || maxValue) search.value = {};
+
+  if (minValue) search.value.$gte = minValue;
+  if (maxValue) search.value.$lte = maxValue;
+
+  return search;
 }
 
 export default ExpenseController;
