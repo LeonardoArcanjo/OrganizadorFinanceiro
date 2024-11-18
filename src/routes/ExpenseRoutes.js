cf6848341839a0fd9989d759bc0c5075148f0f4c
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2024-11-18 15:27:49 -0400

    feat: create pagination middleware to reuse code in search expense, with and no filter applied

--- src/routes/ExpenseRoutes.js
+++ src/routes/ExpenseRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import ExpenseController from "../Controllers/ExpenseController.js";
+import paginator from "../Middleware/Pagination.js";
 
 const router = express.Router();
 
 router
-  .get("/expense", ExpenseController.getAllExpenses)
-  .get("/expense/search", ExpenseController.searchExpense)
+  .get("/expense", ExpenseController.getAllExpenses, paginator)
+  .get("/expense/search", ExpenseController.searchExpense, paginator)
   .get("/expense/:id", ExpenseController.getExpenseById)
   .post("/expense", ExpenseController.insertExpense)
   .put("/expense/:id", ExpenseController.updateExpense)
