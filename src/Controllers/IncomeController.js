697ea91743fa7e49e27bb1f92dc29e323116ce17
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2025-02-22 17:08:25 -0400

    refactor: income controller to accept pagination, sorting and error handler request

--- src/Controllers/IncomeController.js
+++ src/Controllers/IncomeController.js
@@ -1,13 +1,15 @@
 import income from "../Context/Income.js";
-import { CREATE_STATUS, OK_STATUS } from "../Models/Constants.js";
+import NotFound from "../Errors/NotFound.js";
+import { CREATE_STATUS, NO_CONTENT, OK_STATUS } from "../Models/Constants.js";
 
 class IncomeController {
   static async getAllIncomes(req, res, next) {
     try {
-      const incomeList = await income.find({});
-      res.status(OK_STATUS).json(incomeList);
+      const incomeList = income.find();
+      req.response = incomeList;
+      next();
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
@@ -16,19 +18,27 @@ class IncomeController {
       const id = req.params.id;
       const incomeFound = await income.findById(id);
 
-      res.status(OK_STATUS).json(incomeFound);
+      if (incomeFound !== null) {
+        res.status(OK_STATUS).json(incomeFound);
+      } else {
+        next(new NotFound("Income not found!"));
+      }
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
-  static async getIncomeByCategory(req, res, next) {
+  static async searchIncome(req, res, next) {
     try {
-      const category = req.params.category;
-      const incomeByCategoryList = await income.find({
-        category: `${category}`,
-      });
-      res.status(OK_STATUS).json(incomeByCategoryList);
+      let search = searchQueryHandler(req.query);
+
+      if (search !== null) {
+        const result = income.find(search);
+        req.response = result;
+        next();
+      } else if (result.length === 0) {
+        res.status(NO_CONTENT).json(result);
+      }
     } catch (error) {
       next(error);
     }
@@ -37,10 +47,15 @@ class IncomeController {
   static async updateIncome(req, res, next) {
     try {
       const id = req.params.id;
-      await income.findByIdAndUpdate(id, req.body);
-      res.status(OK_STATUS).json({ message: "Income Updated!" });
+      const incomeFound = await income.findByIdAndUpdate(id, req.body);
+
+      if (incomeFound !== null) {
+        res.status(OK_STATUS).json({ message: "Income Updated!" });
+      } else {
+        next(new NotFound("Income not found!"));
+      }
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
@@ -58,12 +73,30 @@ class IncomeController {
   static async deleteIncome(req, res, next) {
     try {
       const id = req.params.id;
-      await income.findByIdAndDelete(id);
-      res.status(OK_STATUS).json({ message: "Income deleted!" });
+      const incomeFound = await income.findByIdAndDelete(id);
+
+      if (incomeFound !== null) {
+        res.status(OK_STATUS).json({ message: "Income deleted!" });
+      } else {
+        next(new NotFound("Income not found!"));
+      }
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
+}
+
+function searchQueryHandler(params) {
+  const { name, category, minValue, maxValue } = params;
+
+  let search = {};
+
+  if (name) search.name = { $regex: name, $options: "i" };
+  if (category) search.category = category;
+  if (minValue || maxValue) search.value = {};
+
+  if (minValue) search.value.$gte = minValue;
+  if (maxValue) search.value.$lte = maxValue;
 }
 
 export default IncomeController;
--- src/routes/IncomeRoutes.js
+++ src/routes/IncomeRoutes.js
@@ -5,9 +5,9 @@ import paginator from "../Middleware/Pagination.js";
 const router = express.Router();
 
 router
-  .get("/income", IncomeController.getAllIncomes)
+  .get("/income", IncomeController.getAllIncomes, paginator)
+  .get("/income/search", IncomeController.searchIncome, paginator)
   .get("/income/:id", IncomeController.getIncomeById)
-  .get("/income/:category", IncomeController.getIncomeByCategory)
   .post("/income", IncomeController.insertIncome)
   .put("/income/:id", IncomeController.updateIncome)
   .delete("/income/:id", IncomeController.deleteIncome);
