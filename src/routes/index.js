43aed13ccf8087952c3d880ceb1631387e0e01f8
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2025-06-22 19:11:47 -0400

    feat: remove credit card bill model, controller and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import expense from "./ExpenseRoutes.js";
 import investment from "./InvestimentRoutes.js";
 import income from "./IncomeRoutes.js";
 import { OK_STATUS } from "../Models/Constants.js";
-import creditCardBill from "./CreditCardBillRoutes.js";
 import creditCardExpense from "./CreditCardExpenseRoutes.js";
 import NotFoundHandler from "../Middleware/NotFoundHandler.js";
 
@@ -12,14 +11,7 @@ const routes = (app) => {
     .route("/")
     .get((req, res) => res.status(OK_STATUS).send("API working..."));
 
-  app.use(
-    express.json(),
-    expense,
-    investment,
-    income,
-    creditCardBill,
-    creditCardExpense
-  );
+  app.use(express.json(), expense, investment, income, creditCardExpense);
 
   app.use(NotFoundHandler);
 };
